0bd50cc66d4a1fd8b45b17f1b29f823f8324448e
Harrison <hmbugi@luc.edu>
2015-06-15 21:51:49 -0500

    improved comments on what the functions are doing

--- data_challenge.py
+++ data_challenge.py
@@ -5,18 +5,20 @@ from csv import *
 
 def socioparser():
     ''' Fetching community area number & name,
-per capita income and hardship index into a list  '''
-    soc_list = []
+per capita income and hardship index into a list
+and return a list containing these values from the csv file '''
+    soc_list = [] 
     soc_obj = open("socio_eco_ind.csv")
     soc_reader = reader(soc_obj)
     for row in soc_reader:
-        soc_list.append([row[0], row[1], row[7], row[8]])
+        soc_list.append([row[0], row[1], row[7], row[8])
     soc_obj.close()
     return soc_list
         
 def birthparser():
-    ''' Fetching community area number and birth rate  '''
-    bir_list = []
+    ''' Fetching community area number and birth rate into a list
+and return a list containing these values from the csv file '''
+    bir_list = [] 
     bir_obj = open("health_ind.csv")
     bir_reader = reader(bir_obj)
     for row in bir_reader:
@@ -25,9 +27,9 @@ def birthparser():
     return bir_list
 
 def deathparser():
-    ''' Fetching community area number, average annual deaths
-and cumulative death ranks '''
-    dea_list = []
+    ''' Fetching community area number and average annual deaths into a list
+and return a list containing these values from the csv file '''
+    dea_list = [] 
     dea_obj = open("death_causes.csv")
     dea_reader = reader(dea_obj)
     for row in dea_reader:
@@ -42,12 +44,11 @@ def populationparser():
     pop_reader = reader(pop_obj)
     for row in pop_reader:
         pop_list.append([row[0], row[2]])
-    pop_obj.close()
     return pop_list
 
 def incomebirth():
     '''Manipulates return values of socioparser() and birthparser()
-and return a list containing values needed for colleration
+and return a list containing values needed for calculating the coefficient of
 income-birth correlation'''
     incomebirth, x, y, xy, x2, y2 = ([] for i in range(6)) #same as [[]]*6 . Both initialize empty lists to variables
     for n in range (1,78): #range start from index 1 avoiding headings and end at index 77
@@ -61,8 +62,8 @@ income-birth correlation'''
 
 def incomedeath():
     '''Manipulates return values of socioparser() and birthparser()
-and return a list containing values needed for colleration
-income-birth correlation'''
+and return a list containing values needed for calculating the coefficient of
+income-death correlation'''
     incomedeath, x, y, xy, x2, y2 = ([] for i in range(6)) #same as [[]]*6 . Both initialize empty lists to variables
     for n in range (1,78): #range start from index 1 avoiding headings and end at index 77
         x.append(int(socioparser()[n][2])) #it is index n-1 because n starts from 1 not 0.
