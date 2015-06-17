7129e69337a2958968b24ea82336368cb53a2d43
Harrison <hmbugi@luc.edu>
2015-06-17 17:45:44 -0500

    changing import statement format

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
+from unittest import *
+from data_challenge import *
 
-import unittest, data_challenge
+class TestFunctions(unittest.TestCase):
+    def test_fileexist(self):
+        '''Tests if all the dataset files exist'''
+        self.assertTrue(fileexist('socio_eco_ind.csv'), True)
+        self.assertTrue(fileexist('health_ind.csv'), True)
+        self.assertTrue(fileexist('death_causes.csv'), True)
+    
+print(fileexist('socio_eco_ind.csv'))
 
-
-
-
+unittest.main()
